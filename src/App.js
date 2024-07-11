63a3ebb0513534430732c5d45116a03de7d89abf
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-11 13:18:29 +0545

    added tailwindcss framework

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1 style={{ color: "white" }}> Good Job !!</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-white"> Good Job !!</h1>;
+    </div>
+  );
 }
 
 export default App;
